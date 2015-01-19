69ea0ad4f2702bbed95cc3a3eec218309faed644
christoph.stadlbauer <christoph.stadlbauer@872ce066-4876-4fca-a6fc-23b94bf576d1>
2015-01-19 02:30:52 +0000

    new version
    git-svn-id: https://dev.geogebra.org/internal/moodle/moodle-qtype_geogebra/trunk@3990 872ce066-4876-4fca-a6fc-23b94bf576d1

--- version.php
+++ version.php
@@ -13,7 +13,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_geogebra';
-$plugin->version = 2014081905;
+$plugin->version = 2014081906;
 
 $plugin->requires = 2014051200;
 $plugin->dependencies = array(
@@ -23,4 +23,4 @@ $plugin->dependencies = array(
 
 $plugin->maturity = MATURITY_RC;
 
-$plugin->release = '0.9.5';
+$plugin->release = '0.9.6';
