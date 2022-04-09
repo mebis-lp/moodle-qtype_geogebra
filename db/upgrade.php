58a4045686f81e8fd64d2ee06bb6db3e5dc4ede9
Philipp Memmel <philipp.memmel@gitlab.mebis.alp.dillingen.de>
2022-04-09 10:49:11 +0200

    MBS-6309: Satisfy phpcs

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,16 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * GeoGebra question type upgrade code.
  *
- * @package        qtype
- * @subpackage     geogebra
+ * @package        qtype_geogebra
  * @author         Christoph Stadlbauer <christoph.stadlbauer@geogebra.org>
  * @copyright  (c) International GeoGebra Institute 2014
  * @license        http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-defined('MOODLE_INTERNAL') || die();
 
 /**
  * Upgrade code for the geogebra question type.
@@ -22,7 +33,7 @@ function xmldb_qtype_geogebra_upgrade($oldversion) {
     global $DB;
     $dbman = $DB->get_manager();
 
-    /// Add a new column newcol to the mdl_geogebra_options
+    // Add a new column newcol to the mdl_geogebra_options.
     if ($oldversion < 2014081908) {
         // Define field isexercise to be added to qtype_geogebra_options.
         $table = new xmldb_table('qtype_geogebra_options');
