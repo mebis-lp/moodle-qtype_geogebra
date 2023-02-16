f0308223c6ac55ae92d4780b8f762104bf0d5e4b
Twingsister <twingsister@gmail.com>
2023-02-17 00:40:10 +0100

    push version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_geogebra';
-$plugin->version = 2024021600;
+$plugin->version = 2024021700;
 $plugin->release = '1.13.3';
 
 $plugin->requires = 2018051703;
